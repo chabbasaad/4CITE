668a6d64894e1bbdeaa643a6ff884a4693e823ca
Chabba Saad <dr.saadchabba@hotmail.fr>
2025-03-09 12:28:52 +0100

    Delete front-end directory

--- front-end/src/service/servises/userSerivice.tsx
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-import {User} from "../model/model.tsx";
-
-const API_URL = "https://jsonplaceholder.typicode.com/users";
-
-export const getUsers = async (): Promise<User[]> => {
-    const response = await axios.get<User[]>(API_URL);
-    return response.data;
-};
